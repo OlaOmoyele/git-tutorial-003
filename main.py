2a558445e9e97b417a9da91564480548dfe40e62
Ola <olaomoyele2@gmail.com>
2025-05-15 09:24:49 +0100

    feat: update main.py to use classes

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ import utilities
 program_name: str = pathlib.Path(__file__).name
 program_version: str = '1.1.0'
 
-if __name__ == '__main__':
-    print(f'Running {program_name} v{program_version}')
-    utilities.welcome_msg()
+class Main():
 
-    print()
-    print('End of Script')
-    print('-'*30)
+    def __init__(self) -> None:
+        print(f'Running {program_name} v{program_version}')
+        utilities.welcome_msg()
+
+    def quit(self) -> None:
+        print()
+        print('End of Script')
+        print('-'*30)
+
+if __name__ == '__main__':
+    main = Main()
+    main.quit()
