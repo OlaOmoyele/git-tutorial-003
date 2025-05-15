3ef74129302ff9372dbc1e8f3ff343a48f4a3207
Ola <olaomoyele2@gmail.com>
2025-05-15 08:50:39 +0100

    feat: add exit message to script

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ program_version: str = '1.1.0'
 if __name__ == '__main__':
     print(f'Running {program_name} v{program_version}')
     utilities.welcome_msg()
+
+    print()
+    print('End of Script')
+    print('-'*30)
